dbedea52eed4f2c299ab006efc9e9ceb34c89bff
duynv4121 <duysangtao2001@gmail.com>
2022-09-02 08:56:38 +0700

    upsource

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
-const nameGirl = 'Em';
+const nameGirl = 'Chị';
 const giftUrl = 'http://nodemy.vn';
-const eventName = 'Chúc Mừng 20-10';
-const titleCard = 'Tặng người ấy';
-const contentCard = 'Chúc honey của anh 20/10 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau';
+const eventName = 'Món quà 2-9 tặng';
+const titleCard = 'Gửi cho Chị Mít Ướt';
+const contentCard = 'Chúc chị Mít Ướt có ngày 2-9 vui vẻ nhaaa chị ơi. Chúc chị những ngày tháng này mạnh mẽ đúng lúc, yếu đuối đúng khi. Đừng cố gắng nhiều quá. Năm tháng qua chị đã vất vả nhiều rồi, mạnh mẽ lên nào bà zà ưiiiiiiii. Xin lỗi về đôi lúc đã nóng giận với chị nhaaaa, không hiểu chị. Mong là sau này một cuộc sống tràn đầy niềm vui hạnh phúc sẽ đến với chị nhaaa, đừng quên có gì cứ chia sẻ với em nhaaaa, bởi vì sẽ luôn có em bên cạnh chị lúc chị cần mà. Chốt nhaaa! Chúc chị tất cả ngày 2-9 💘💘💘';
 
 // phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
 // Cần hỗ trợ hãy liên hệ: 
 // Mr-Nam http://facebook.com/nam.nodemy
 // Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
-const giftImage = 'hot-girl.png';
+const giftImage = './img/anh1.jpg';
 const base64 = '';
 const giftImageBase64 = "data:image/png;base64, " + base64;
